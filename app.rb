f800d83cc0aa348b7e94d777c2bb9fe27ee5c859
Daniel Watts <d.watts15@outlook.com>
2022-02-03 16:37:57 +0000

    Up to 'switching_turns' challenge

---
+++ app.rb
@@ -0,0 +1,38 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+require './lib/game'
+require './lib/player'
+
+class Battle < Sinatra::Base
+  enable :sessions
+
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/names' do
+    player_1 = Player.new(params[:player_1_name])
+    player_2 = Player.new(params[:player_2_name])
+    $game = Game.new(player_1, player_2)
+    redirect '/play'
+  end
+
+  get '/play' do
+    @game = $game
+    erb :play
+  end
+
+  get '/attack' do
+    @game = $game
+    @game.attack(@game.player_2)
+    erb :attack
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+
+end
